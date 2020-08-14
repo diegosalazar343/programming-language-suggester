812febc54d5dff1533b896af06697ac837304b8a
diegosalazar343 <salazardiego343@gmail.com>
2020-08-14 10:26:43 -0700

    creat else, else if, and if

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function determineResult(input1, input2, input3, input4,input5) {
+
+  const valTotal = input1 + input2 + input3 + input4 + input5;
+
+  if (valTotal <==7) {
+    language = "C#"
+  }
+  else if (valTotal<==12) {
+    language = "JavaScript"
+  }
+  else{
+    language = "Ruby"
+  }
+  return language;
+}
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const
+  }
+}
