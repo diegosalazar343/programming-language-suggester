bfa58dc5db954c005cff9eff33be82144386d3ca
diegosalazar343 <salazardiego343@gmail.com>
2020-08-14 12:34:38 -0700

    add photo as answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function determineResult(input1, input2, input3, input4,input5) {
   const valTotal = input1 + input2 + input3 + input4 + input5;
 
   if (valTotal <= 7) {
-    language = "C#"
+    language = "C#" ;
   }
   else if (valTotal<= 12) {
     language = "JavaScript"
@@ -17,12 +17,12 @@ function determineResult(input1, input2, input3, input4,input5) {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    const interests = parseInt($("input=radio[name=interests]:checked").val());
-    const animal = parseInt($("input=radio[name=animal]:checked").val());
-    const learning = parseInt($("input=radio[name=learning]:checked").val());
-    const sport = parseInt($("input=radio[name=sport]:checked").val());
-    const weaknesses = parseInt($("input=radio[name=weaknesses]:checked").val());
-    let languageResult = determineResult(interests,animal,learning,sport,weakesses);
+    const interests = parseInt($("input:radio[name=interests]:checked").val());
+    const animal = parseInt($("input:radio[name=animal]:checked").val());
+    const learning = parseInt($("input:radio[name=learning]:checked").val());
+    const sport = parseInt($("input:radio[name=sport]:checked").val());
+    const weaknesses = parseInt($("input:radio[name=weaknesses]:checked").val());
+    let languageResult = determineResult(interests,animal,learning,sport,weaknesses);
 
     if (languageResult === "C#") {
       $("#quizResults").hide();
